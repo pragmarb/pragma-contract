1b8fac533969ac397c7cd264ef431f4be9883199
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-15 18:36:42 +0100

    Create project structure

---
+++ lib/pragma/contract.rb
@@ -0,0 +1,7 @@
+require "pragma/contract/version"
+
+module Pragma
+  module Contract
+    # Your code goes here...
+  end
+end
---
+++ lib/pragma/contract/version.rb
@@ -0,0 +1,5 @@
+module Pragma
+  module Contract
+    VERSION = "0.1.0"
+  end
+end
