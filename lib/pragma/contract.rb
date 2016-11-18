90307e04ce2182444ee2458e1266f451ab7ac34e
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-18 13:38:18 +0100

    Fix docs for Pragma::Contract module

--- lib/pragma/contract.rb
+++ lib/pragma/contract.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 require 'pragma/contract/version'
 
-# Form objects on steroids for your HTTP API.
-#
-# @author Alessandro Desantis
 module Pragma
+  # Form objects on steroids for your HTTP API.
+  #
+  # @author Alessandro Desantis
   module Contract
   end
 end
